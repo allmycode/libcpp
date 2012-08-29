f82f3f3a3feaaabf727580cb05112b95da0c11fc
Stas Lyahnovich <stas.lyahnovich@gmail.com>
2012-08-30 01:03:31 +0400

    Execution time measurer

---
+++ measurer.h
@@ -0,0 +1,13 @@
+// Execution time measurer
+#include <ctime>
+
+class measurer {
+	clock_t start;
+
+public:
+	measurer(): start(clock()) {}
+
+	double seconds() {
+		return (double)(clock() - start)/CLOCKS_PER_SEC;
+	}
+};
